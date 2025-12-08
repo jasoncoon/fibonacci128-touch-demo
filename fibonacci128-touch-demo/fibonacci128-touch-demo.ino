a5a32e87b41532ad98fcc4f5d2556ae42a52d4a4
jasoncoon <3598755+jasoncoon@users.noreply.github.com>
2025-12-08 17:32:19 -0600

    Add pattern list, switch with reset button. Update FastLED.

--- fibonacci128-touch-demo/fibonacci128-touch-demo.ino
+++ fibonacci128-touch-demo/fibonacci128-touch-demo.ino
@@ -17,7 +17,8 @@
 */
 
 #include <FastLED.h>  // https://github.com/FastLED/FastLED
-#include "Adafruit_FreeTouch.h" //https://github.com/adafruit/Adafruit_FreeTouch
+#include "Adafruit_FreeTouch.h" // https://github.com/adafruit/Adafruit_FreeTouch
+#include <FlashStorage_SAMD.h> // https://github.com/khoih-prog/FlashStorage_SAMD
 #include "GradientPalettes.h"
 
 FASTLED_USING_NAMESPACE
@@ -32,9 +33,11 @@ FASTLED_USING_NAMESPACE
 #define MILLI_AMPS         1400 // IMPORTANT: set the max milli-Amps of your power supply (4A = 4000mA)
 #define FRAMES_PER_SECOND  120
 
+#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
+
 CRGB leds[NUM_LEDS];
 
-uint8_t brightness = 32;
+uint8_t brightness = 64;
 
 Adafruit_FreeTouch touch0 = Adafruit_FreeTouch(A3, OVERSAMPLE_4, RESISTOR_0, FREQ_MODE_NONE);
 Adafruit_FreeTouch touch1 = Adafruit_FreeTouch(A6, OVERSAMPLE_4, RESISTOR_0, FREQ_MODE_NONE);
@@ -64,7 +67,7 @@ boolean activeWaves = false;
 
 // Forward declarations of an array of cpt-city gradient palettes, and
 // a count of how many there are.
-extern const TProgmemRGBGradientPalettePtr gGradientPalettes[];
+extern const TProgmemRGBGradientPaletteRef gGradientPalettes[];
 
 uint8_t gCurrentPaletteNumber = 0;
 
@@ -76,6 +79,14 @@ CRGBPalette16 gTargetPalette( gGradientPalettes[0] );
 uint8_t secondsPerPalette = 10;
 
 static uint8_t hue = 0;
+
+#include "Patterns.h"
+
+typedef void (*SimplePatternList[])();
+SimplePatternList patterns = { colorWavesFibonacci, prideFibonacci, outwardPalettes, rotatingPalettes, outwardRainbow, rotatingRainbow };
+
+uint8_t currentPatternIndex = 0;
+const uint8_t patternCount = ARRAY_SIZE(patterns);
 
 void setup() {
   Serial.begin(115200);
@@ -97,6 +108,8 @@ void setup() {
   FastLED.show();
 
   FastLED.setBrightness(brightness);
+
+  readEeprom();
 }
 
 void loop() {
@@ -123,15 +136,8 @@ void loop() {
     hue++;
   }
 
-  if (!activeWaves){
-    colorWavesFibonacci();
-    // prideFibonacci();
-    // colorTest();
-    // horizontalRainbow();
-    // verticalRainbow();
-    // diagonalRainbow();
-    // outwardRainbow();
-    // rotatingRainbow();
+  if (!activeWaves) {
+    patterns[currentPatternIndex]();
   }
 
   touchDemo();
@@ -142,6 +148,38 @@ void loop() {
 
   // insert a delay to keep the framerate modest
   FastLED.delay(1000 / FRAMES_PER_SECOND);
+}
+
+const int WRITTEN_SIGNATURE = 0xBEEFDEED;
+
+void readEeprom() {
+  Serial.print("EEPROM length: ");
+  Serial.println(EEPROM.length());
+
+  uint16_t address = 0;
+  int number;
+
+  // Read the content of emulated-EEPROM
+  EEPROM.get(address, number);
+
+  if (number >= patternCount) number = 0;
+
+  currentPatternIndex = number;
+
+  // Print the current number on the serial monitor
+  Serial.print("Number = 0x");
+  Serial.println(number, HEX);
+
+  // Save into emulated-EEPROM the number increased by 1 for the next run of the sketch
+  EEPROM.put(address, (int) (number + 1));
+
+  if (!EEPROM.getCommitASAP())
+  {
+    Serial.println("CommitASAP not set. Need commit()");
+    EEPROM.commit();
+  }
+
+  Serial.println("Done writing to emulated EEPROM.");
 }
 
 bool touchChanged = true;
@@ -305,156 +343,5 @@ void touchDemo() {
 
     // draw waves starting from the corner closest to each touch sensor
     drawCircle(x, y, radii[i], color, 4);
-  }
-}
-
-// ColorWavesWithPalettes by Mark Kriegsman: https://gist.github.com/kriegsman/8281905786e8b2632aeb
-// This function draws color waves with an ever-changing,
-// widely-varying set of parameters, using a color palette.
-void fillWithColorWaves(CRGB* ledarray, uint16_t numleds, CRGBPalette16& palette, bool useFibonacciOrder) {
-  static uint16_t sPseudotime = 0;
-  static uint16_t sLastMillis = 0;
-  static uint16_t sHue16 = 0;
-
-  // uint8_t sat8 = beatsin88( 87, 220, 250);
-  uint8_t brightdepth = beatsin88( 341, 96, 224);
-  uint16_t brightnessthetainc16 = beatsin88( 203, (25 * 256), (40 * 256));
-  uint8_t msmultiplier = beatsin88(147, 23, 60);
-
-  uint16_t hue16 = sHue16;//gHue * 256;
-  uint16_t hueinc16 = beatsin88(113, 300, 1500);
-
-  uint16_t ms = millis();
-  uint16_t deltams = ms - sLastMillis ;
-  sLastMillis  = ms;
-  sPseudotime += deltams * msmultiplier;
-  sHue16 += deltams * beatsin88( 400, 5, 9);
-  uint16_t brightnesstheta16 = sPseudotime;
-
-  for ( uint16_t i = 0 ; i < numleds; i++) {
-    hue16 += hueinc16;
-    uint8_t hue8 = hue16 / 256;
-    uint16_t h16_128 = hue16 >> 7;
-    if ( h16_128 & 0x100) {
-      hue8 = 255 - (h16_128 >> 1);
-    } else {
-      hue8 = h16_128 >> 1;
-    }
-
-    brightnesstheta16 += brightnessthetainc16;
-    uint16_t b16 = sin16( brightnesstheta16  ) + 32768;
-
-    uint16_t bri16 = (uint32_t)((uint32_t)b16 * (uint32_t)b16) / 65536;
-    uint8_t bri8 = (uint32_t)(((uint32_t)bri16) * brightdepth) / 65536;
-    bri8 += (255 - brightdepth);
-
-    uint8_t index = hue8;
-    //index = triwave8( index);
-    index = scale8( index, 240);
-
-    CRGB newcolor = ColorFromPalette( palette, index, bri8);
-
-    uint16_t pixelnumber = i;
-
-    if (useFibonacciOrder) pixelnumber = fibonacciToPhysical[i];
-
-    pixelnumber = (numleds - 1) - pixelnumber;
-
-    nblend(ledarray[pixelnumber], newcolor, 128);
-  }
-}
-
-void colorWavesFibonacci() {
-  fillWithColorWaves(leds, NUM_LEDS, gCurrentPalette, true);
-}
-
-// Pride2015 by Mark Kriegsman: https://gist.github.com/kriegsman/964de772d64c502760e5
-// This function draws rainbows with an ever-changing,
-// widely-varying set of parameters.
-void fillWithPride(bool useFibonacciOrder)
-{
-  static uint16_t sPseudotime = 0;
-  static uint16_t sLastMillis = 0;
-  static uint16_t sHue16 = 0;
-
-  // uint8_t sat8 = beatsin88( 87, 220, 250);
-  uint8_t sat8 = beatsin88( 43.5, 220, 250);
-  // uint8_t brightdepth = beatsin88( 341, 96, 224);
-  uint8_t brightdepth = beatsin88(171, 96, 224);
-  // uint16_t brightnessthetainc16 = beatsin88( 203, (25 * 256), (40 * 256));
-  uint16_t brightnessthetainc16 = beatsin88( 102, (25 * 256), (40 * 256));
-  // uint8_t msmultiplier = beatsin88(147, 23, 60);
-  uint8_t msmultiplier = beatsin88(74, 23, 60);
-
-  uint16_t hue16 = sHue16;//gHue * 256;
-  // uint16_t hueinc16 = beatsin88(113, 1, 3000);
-  uint16_t hueinc16 = beatsin88(57, 1, 128);
-
-  uint16_t ms = millis();
-  uint16_t deltams = ms - sLastMillis ;
-  sLastMillis  = ms;
-  sPseudotime += deltams * msmultiplier;
-  // sHue16 += deltams * beatsin88( 400, 5, 9);
-  sHue16 += deltams * beatsin88( 200, 5, 9);
-  uint16_t brightnesstheta16 = sPseudotime;
-
-  for ( uint16_t i = 0 ; i < NUM_LEDS; i++) {
-    hue16 += hueinc16;
-    uint8_t hue8 = hue16 / 256;
-
-    brightnesstheta16  += brightnessthetainc16;
-    uint16_t b16 = sin16( brightnesstheta16  ) + 32768;
-
-    uint16_t bri16 = (uint32_t)((uint32_t)b16 * (uint32_t)b16) / 65536;
-    uint8_t bri8 = (uint32_t)(((uint32_t)bri16) * brightdepth) / 65536;
-    bri8 += (255 - brightdepth);
-
-    CRGB newcolor = CHSV( hue8, sat8, bri8);
-
-    uint16_t pixelnumber = i;
-
-    if (useFibonacciOrder) pixelnumber = fibonacciToPhysical[i];
-
-    pixelnumber = (NUM_LEDS - 1) - pixelnumber;
-
-    nblend( leds[pixelnumber], newcolor, 64);
-  }
-}
-
-void prideFibonacci() {
-  fillWithPride(true);
-}
-
-void colorTest() {
-  fill_solid(leds, NUM_LEDS, CHSV(hue, 255, 255));
-}
-
-void horizontalRainbow() {
-  for (uint16_t i = 0; i < NUM_LEDS; i++) { 
-    leds[i] = CHSV(coordsX[i] + hue, 255, 255);
-  }
-}
-
-void verticalRainbow() {
-  for (uint16_t i = 0; i < NUM_LEDS; i++) { 
-    leds[i] = CHSV(coordsY[i] + hue, 255, 255);
-  }
-}
-
-void diagonalRainbow() {
-  for (uint16_t i = 0; i < NUM_LEDS; i++) { 
-    leds[i] = CHSV(coordsX[i] + coordsY[i] + hue, 255, 255);
-  }
-}
-
-void outwardRainbow() {
-  for (uint16_t i = 0; i < NUM_LEDS; i++) { 
-    leds[i] = CHSV(radius[i] - hue, 255, 255);
-  }
-}
-
-void rotatingRainbow() {
-  for (uint16_t i = 0; i < NUM_LEDS; i++) { 
-    leds[i] = CHSV(angles[i] - hue, 255, 255);
   }
 }
